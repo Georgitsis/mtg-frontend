00d1a433c80b85c932577d53383d3523e1091e6d
PsyChoBoyJAck83 <georgitsis.theodoros@gmail.com>
2024-04-19 23:42:14 +0200

    main page layout

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,7 +1,14 @@
 import "./main-page.css";
 
 function MainPage() {
-  return <div className="main-view">MainView</div>;
+  return (
+    <div className="main-page">
+      <div className="header">Header</div>
+      <div className="sidebar">Sidebar</div>
+      <div className="content">Content</div>
+      <div className="footer">Footer</div>
+    </div>
+  );
 }
 
 export default MainPage;
