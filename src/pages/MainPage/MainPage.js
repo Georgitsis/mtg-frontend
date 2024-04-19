635d9da8b52fa6f29e54aae5b9cfd0aaeea97149
PsyChoBoyJAck83 <georgitsis.theodoros@gmail.com>
2024-04-20 00:30:57 +0200

    className change sidebar -> search-element

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -4,7 +4,7 @@ function MainPage() {
   return (
     <div className="main-page">
       <div className="header">Header</div>
-      <div className="sidebar">Sidebar</div>
+      <div className="search-element">Sidebar</div>
       <div className="content">Content</div>
       <div className="footer">Footer</div>
     </div>
