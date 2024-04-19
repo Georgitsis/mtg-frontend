0191fa16801436b33009df923f1cf21156a0f3bc
Theo Georgitsis <98169347+Georgitsis@users.noreply.github.com>
2024-04-20 00:34:46 +0200

    Main page (#1)
    * added main theme scss file
    
    * main page layout
    
    * responsive design when wider then extra-small-devices
    
    * className change sidebar -> search-element
    
    * small correction

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,7 +1,14 @@
-//import "./App.css";
+import "./main-page.css";
 
 function MainPage() {
-  return <div className="main-view">MainView</div>;
+  return (
+    <div className="main-page">
+      <div className="header">Header</div>
+      <div className="search-element"></div>
+      <div className="content">Content</div>
+      <div className="footer">Footer</div>
+    </div>
+  );
 }
 
 export default MainPage;
