8e08d968e0a0c78b6b028393f07fe5ad4665721c
PsyChoBoyJAck83 <georgitsis.theodoros@gmail.com>
2024-04-20 02:33:37 +0200

    searchCompoent passes qyuerrySttring to mainPage and start corresponding fucntion

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
+import SearchComponent from "../../components/SearchComponent/SearchComponent";
 import "./main-page.css";
 
 function MainPage() {
+  const handleQuerry = (querryString) => {
+    console.log("handleQuerry in MainPage: ", querryString);
+  };
+
+  useEffect(() => {});
+
   return (
     <div className="main-page">
       <div className="header">Header</div>
-      <div className="search-element"></div>
+      <SearchComponent className="search-element" handleQuerry={handleQuerry} />
       <div className="content">Content</div>
       <div className="footer">Footer</div>
     </div>
