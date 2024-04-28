360774ae668e3492edf9503e91439a785042bb68
PsyChoBoyJAck83 <georgitsis.theodoros@gmail.com>
2024-04-28 16:21:55 +0200

    split card header into card name and mancosts

--- src/components/CardWithTextComponent/CardWithTextComponent.js
+++ src/components/CardWithTextComponent/CardWithTextComponent.js
@@ -4,11 +4,14 @@ import noImage from "../../assets/images/NoImage.png";
 
 function CardWithTextComponent(props) {
   const { card, className } = props;
-  console.log(card.imageUrl);
+  console.log(card);
   return (
     <li className={className}>
       <img src={card.imageUrl ? card.imageUrl : noImage} alt={"Image of " + card.name} className="card-image" />
-      <div className="card-header">{card.name}</div>
+      <div className="card-header">
+        <div className="card-name">{card.name}</div>
+        <ManaCost />
+      </div>
       <div className="card-type">{card.type}</div>
       <div className="card-text">{card.text}</div>
       {card.power ? <div className="card-pow-tough">{card.power + " / " + card.toughness}</div> : <></>}
